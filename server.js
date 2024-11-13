afef9523ebe07dc3fce2795f26105182fa84b53d
Shaaz <asadakhtar800@gmail.com>
2024-11-13 13:39:16 +0530

    check routing

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const app = express();
 
 // Define a route to handle requests to the home route
 app.get('/', (req, res) => {
-  res.send('Hello World from Node.js and Express!');
+  res.send('Hello World from Node.js and Express! This is Home page');
+});
+app.get('/about', (req, res) => {
+  res.send('This is About Page');
 });
 
 // Start the server
-const PORT = 5000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
